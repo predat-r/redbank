495cd3d4f8476afc808ab250cc7175d9d243c903
Zohaib <zohaib.codemaster@gmail.com>
2026-08-03 14:57:16 +0500

    wip

--- src/main/java/com/redmath/redbank/account_holder/AccountHolderRepository.java
+++ src/main/java/com/redmath/redbank/account_holder/AccountHolderRepository.java
@@ -5,17 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Spring Data JPA Repository for {@link AccountHolder} entity.
- */
 @Repository
 public interface AccountHolderRepository extends JpaRepository<AccountHolder, Long> {
-
-    /**
-     * Find the account holder associated with a given user.
-     *
-     * @param user the user
-     * @return an Optional containing the found account holder, or empty
-     */
     Optional<AccountHolder> findByUser(User user);
+
+    Optional<AccountHolder> findByAccountNumber(String accountNumber);
 }
