09a0f9da4ca4d45414311fe2558ed5f8c8cce487
Zohaib <zohaib.codemaster@gmail.com>
2026-08-04 10:27:42 +0500

    remove duplicate code

--- src/main/java/com/redmath/redbank/account_holder/AccountHolderRepository.java
+++ src/main/java/com/redmath/redbank/account_holder/AccountHolderRepository.java
@@ -7,11 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AccountHolderRepository extends JpaRepository<AccountHolder, Long> {
 
   Optional<AccountHolder> findByUserId(Long userId);
-
-  Optional<AccountHolder> findByUser(User user);
-
-  Optional<AccountHolder> findByAccountNumber(String accountNumber);
-
+  
   AccountHolder getAccountHoldersById(Long id);
 
   AccountHolder getAccountHoldersByAccountNumber(String accountNumber);
