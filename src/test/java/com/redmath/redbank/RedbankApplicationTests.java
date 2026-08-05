6797a0b10d65a7a264860572f5fdc96ad14ffd09
Zohaib <zohaib.codemaster@gmail.com>
2026-08-05 10:52:47 +0500

    fix: add test profile in test application

--- src/test/java/com/redmath/redbank/RedbankApplicationTests.java
+++ src/test/java/com/redmath/redbank/RedbankApplicationTests.java
@@ -2,12 +2,14 @@ package com.redmath.redbank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RedbankApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
