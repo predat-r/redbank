c1fb4ef22a2d076ece8b7ea3f338f5a7253fe75a
Ahmad Tariq <dev.ahmadtariq@gmail.com>
2026-08-13 10:12:29 +0500

    fix(chatbot): fix context issue in chatbot

--- src/main/java/com/redmath/redbank/ai/config/ChatClientConfig.java
+++ src/main/java/com/redmath/redbank/ai/config/ChatClientConfig.java
@@ -3,10 +3,8 @@ package com.redmath.redbank.ai.config;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
-import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
-import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,7 +27,12 @@ public class ChatClientConfig {
    */
   @Bean("intentChatClient")
   ChatClient intentChatClient(ChatClient.Builder builder) {
-    return builder.build();
+    ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+    Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+
+    return builder
+        .defaultAdvisors(memoryAdvisor)
+        .build();
   }
 
   /**
