56aab897653764f2bb0af4b0503131d8a6414521
Zohaib <zohaib.codemaster@gmail.com>
2026-08-20 17:30:34 +0500

    fix spotbug issues

--- src/main/java/com/redmath/redbank/statement/StatementGenerationListener.java
+++ src/main/java/com/redmath/redbank/statement/StatementGenerationListener.java
@@ -1,6 +1,5 @@
 package com.redmath.redbank.statement;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
@@ -14,7 +13,6 @@ public class StatementGenerationListener {
 
   private final StatementProcessorService statementProcessorService;
 
-  @SuppressFBWarnings("EI_EXPOSE_REP2")
   public StatementGenerationListener(StatementProcessorService statementProcessorService) {
     this.statementProcessorService = statementProcessorService;
   }
