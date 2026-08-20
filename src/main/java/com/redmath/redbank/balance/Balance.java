d31fa4e404137abbd81180b9f05422cb67af3792
Zohaib <zohaib.codemaster@gmail.com>
2026-08-20 17:48:43 +0500

    add transaction_id index to Balance entity

--- src/main/java/com/redmath/redbank/balance/Balance.java
+++ src/main/java/com/redmath/redbank/balance/Balance.java
@@ -9,6 +9,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -24,7 +25,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "balance")
+@Table(name = "balance", indexes = {
+    @Index(name = "idx_balance_transaction_id", columnList = "transaction_id")
+})
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Balance {
