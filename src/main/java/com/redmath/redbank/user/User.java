05ed00af456c8c21efeff0d3a6db6dd68b28e9c5
Zohaib <zohaib.codemaster@gmail.com>
2026-08-20 17:49:09 +0500

    add approved_by_user_id index to User entity

--- src/main/java/com/redmath/redbank/user/User.java
+++ src/main/java/com/redmath/redbank/user/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -21,7 +22,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @Builder
-@Table(name = "users")
+@Table(name = "users", indexes = {
+    @Index(name = "idx_users_approved_by_user_id", columnList = "approved_by_user_id")
+})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class User {
